c4fdb8e547200ee5247d8325a1347f32f87b946b
MatiGP <mateusz.swieca@o2.pl>
2021-05-01 11:52:08 +0200

    Added to enemies ability to use multiple attacks, redone some inventory scripts

---
+++ SmallBraveHero/Assets/Scripts/Editor/ShowMultipleAttacsArray.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using Code.StateMachine.AI.Actions;
+
+[CustomEditor(typeof(AttackThePlayer))]
+public class ShowMultipleAttacsArray : Editor
+{
+    Rect size = new Rect(0, 0, 100, 400);
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        EditorGUILayout.BeginVertical();
+
+        AttackThePlayer attackThePlayer = target as AttackThePlayer;
+
+        attackThePlayer.isRanged = GUILayout.Toggle(attackThePlayer.isRanged, "Is Ranged");
+
+        attackThePlayer.hasMultipleAttacks = GUILayout.Toggle(attackThePlayer.hasMultipleAttacks, "Has Multiple Attacks");
+
+        EditorGUILayout.BeginHorizontal();
+        if (attackThePlayer.hasMultipleAttacks)
+        {            
+            attackThePlayer.attackCount = EditorGUILayout.IntField("Attack Count: ", attackThePlayer.attackCount);           
+        }
+        else
+        {
+            attackThePlayer.attackCount = 0;
+        }
+        EditorGUILayout.EndHorizontal();
+        EditorGUILayout.EndVertical();
+    }
+}
--- SmallBraveHero/Assets/Scripts/StateMachine/AI/Actions/AttackThePlayer.cs
+++ SmallBraveHero/Assets/Scripts/StateMachine/AI/Actions/AttackThePlayer.cs
@@ -7,12 +7,32 @@ namespace Code.StateMachine.AI.Actions
 {
     [CreateAssetMenu(menuName = "AI/Actions/AttackThePlayer")]
     public class AttackThePlayer : AIAction
-    {
+    { 
+        [HideInInspector()]
+        public bool isRanged;
+        [HideInInspector()]
+        public bool hasMultipleAttacks;
+        [HideInInspector()]
+        public int attackCount;
+
         public override void Act(AIController controller)
         {
             Debug.Log($"Attacking :{controller.Target.name}");
 
-            controller.CharacterAnimator.Play(controller.CharacterAnimPrefix + EAction.MeleeAttack);
+            if (isRanged)
+            {
+                //TODO: IMPLEMENT ME PLS
+            }
+            else
+            {
+                if (hasMultipleAttacks)
+                {
+                    controller.CharacterAnimator.Play(controller.CharacterAnimPrefix + EAction.MeleeAttack + Random.Range(0, attackCount+1));
+                }
+            }
+            
         }
     }
+
+
 }
