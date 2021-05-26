d950005d779d51145e82dbc7787d76bbdbc95964
Ka9eR <kcpr.parol2@gmail.com>
2021-05-26 20:15:12 +0200

    Added Trap Sprites: Stone, Flamethrower

---
+++ SmallBraveHero/Assets/Scripts/Traps/FallingRock.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingRock : MonoBehaviour
+{
+    Rigidbody2D siema;
+
+    private void Start()
+    {
+        siema = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Equals("Player"))
+            siema.isKinematic = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name.Equals("Player"))
+            Debug.Log("Player killed");
+    }
+}
